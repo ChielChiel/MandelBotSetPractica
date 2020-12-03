603470bd553598d71e61da2871a8d29feb2bb687
Michiel Schouten <51763911+ChielChiel@users.noreply.github.com>
2020-12-03 13:01:34 +0100

    Kleuren toegevoegd aan form.cs
    Je kan nu kiezen welke kleuren combinatie je als gebruiker wil zien. Moet je wel ff op submit klikken.  Vanaf lijn 52 moeten we nog per keuze instellen wat de scalefactor, Midpoint en MaxLoop is. vanaf lijn 195 worden de kleuren gedaan. Met "HSL2RGB" is wel leuk om mee te kloten qua kleuren

--- MandelBotSetPractica/Form.cs
+++ MandelBotSetPractica/Form.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,11 +13,10 @@ namespace MandelBotSetPractica
 {
     public partial class Form : System.Windows.Forms.Form
     {
-
         private Size MandelBrotSize = new Size(400, 400);
         //relatief tot the mandelbrotImg
         private Point MidPoint = new Point(0, 0);
-
+        string ColorMode = "default";
         private Double ScaleFactor = 1;
         private int MaxLoop = 100;
         Double MapXFrom;
@@ -33,32 +32,66 @@ namespace MandelBotSetPractica
         {
             //Here we set the standard values into the text boxes
             MidXText.Text = (MidPoint.X * this.ScaleFactor).ToString();
-            MidYText.Text = (MidPoint.Y *this.ScaleFactor).ToString();
+            MidYText.Text = (MidPoint.Y * this.ScaleFactor).ToString();
             ScaleText.Text = ScaleFactor.ToString();
             MaxText.Text = MaxLoop.ToString();
             this.Width = 800;
             this.Height = 800;
             this.BackColor = Color.White;
-            MapXFrom = 0-MandelBrotSize.Width / 2;
-            MapYFrom = 0-MandelBrotSize.Height / 2;
-            MapXTo = 0+MandelBrotSize.Width / 2;
-            MapYTo = 0+MandelBrotSize.Height / 2;
             MandelBrotImg.Size = this.MandelBrotSize;
             MandelBrotImg.Paint += this.DrawMandelBrot;
             MandelBrotImg.MouseClick += new MouseEventHandler(this.zoom);
-           
-
+            listBox1.Items.Add("default");
+            listBox1.Items.Add("yellow");
+            listBox1.Items.Add("fancy");
+            listBox1.Items.Add("zembla");
+            listBox1.SelectionMode = SelectionMode.One;
+            listBox1.SelectedIndexChanged += this.listBox1_SelectedIndexChanged;
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = listBox1.GetItemText(listBox1.SelectedItem);
+            Console.WriteLine("lit" + selectedItem);
+            //Points, maxloop en scalefactor moeten we dus nog kiezen behalve default dan.
+            switch (selectedItem) {
+                case "default":
+                    this.ScaleFactor = 2;
+                    this.MidPoint = new Point(0, 0);
+                    this.MaxLoop = 100;
+                    break;
+                case "yellow":
+                    this.ScaleFactor = 2;
+                    this.MidPoint = new Point(0, 0);
+                    this.MaxLoop = 100;
+                    break;
+                case "fancy":
+                    this.ScaleFactor = 3;
+                    this.MidPoint = new Point(0, 0);
+                    this.MaxLoop = 100;
+                    break;
+                case "zembla":
+                    this.ScaleFactor = 4;
+                    this.MidPoint = new Point(0, 0);
+                    this.MaxLoop = 100;
+                    break;
+            }
+            this.ColorMode = selectedItem;
+
+            MandelBrotImg.Update();
+        }
+
         void zoom(object sender, MouseEventArgs e)
         {
-            this.ScaleFactor = this.ScaleFactor * 2;
+            this.ScaleFactor = this.ScaleFactor /  2;
             ScaleText.Text = this.ScaleFactor.ToString();
-
-            if (e.X-200 < 0)
+            Console.WriteLine(e.X + "BXXX");
+            Console.WriteLine(e.Y + "YYYY");
+            if (e.X - 200 < 0)
             {
                 MidPoint.X = ((MidPoint.X - (e.X - 200) * -1));
 
-                if (e.Y-200 < 0)
+                if (e.Y - 200 < 0)
                 {
                     MidPoint.Y = ((MidPoint.Y - (e.Y - 200) * -1));
                 }
@@ -84,10 +117,10 @@ namespace MandelBotSetPractica
             MidYText.Text = MidPoint.Y.ToString();
             this.MidPoint = new Point(MidPoint.X, MidPoint.Y);
             Console.WriteLine(this.ScaleFactor + "END ZOOM");
-            
+
             MandelBrotImg.Refresh();
-            
-            
+
+
         }
         void DrawMandelBrot(object sender, PaintEventArgs e)
         {
@@ -100,10 +133,32 @@ namespace MandelBotSetPractica
             Console.WriteLine(this.MidPoint.X);
             Console.WriteLine(this.MidPoint.Y);
             Console.WriteLine(this.ScaleFactor + "SCALLELEE");
-            this.MapXFrom = (this.MidPoint.X - (this.MapXTo - this.MapXFrom));
-            this.MapYFrom = (this.MidPoint.Y - (this.MapYTo - this.MapYFrom));
-            this.MapXTo = (this.MidPoint.X + (MandelBrot.Width / this.ScaleFactor));
-            this.MapYTo = (this.MidPoint.Y + (MandelBrot.Height / this.ScaleFactor));
+            /*
+             this.MapXFrom = (this.MidPoint.X - ((MandelBrot.Width / 2) * 1 / this.ScaleFactor));
+             this.MapYFrom = (this.MidPoint.Y - ((MandelBrot.Height / 2) * 1/  this.ScaleFactor));
+             this.MapXTo = (this.MidPoint.X + ((MandelBrot.Width / 2) * 1 / this.ScaleFactor));
+             this.MapYTo = (this.MidPoint.Y + ((MandelBrot.Height / 2) * 1/ this.ScaleFactor));
+             */
+            /*
+            this.MapXFrom = this.MidPoint.X - this.ScaleFactor * MandelBrot.Width;
+            this.MapXTo = this.MidPoint.X + this.ScaleFactor * MandelBrot.Width;
+
+            this.MapYFrom = this.MidPoint.Y - this.ScaleFactor * MandelBrot.Height;
+            this.MapYTo = this.MidPoint.Y + this.ScaleFactor * MandelBrot.Height;
+            */
+           /*
+            this.MapXFrom = (this.ScaleFactor) * this.MidPoint.X -  MandelBrot.Width;
+            this.MapXTo = (this.ScaleFactor) *  this.MidPoint.X +  MandelBrot.Width;
+
+            this.MapYFrom = (this.ScaleFactor) *  this.MidPoint.Y - MandelBrot.Height;
+            this.MapYTo = (this.ScaleFactor) *  this.MidPoint.Y + MandelBrot.Height;
+           */
+
+            this.MapXFrom = (this.ScaleFactor) * -2.5;
+            this.MapXTo = (this.ScaleFactor) * 2.5;
+
+            this.MapYFrom = (this.ScaleFactor) * -2.5;
+            this.MapYTo = (this.ScaleFactor) * 2.5;
 
             Console.WriteLine(MapXFrom + "MAPXFROM");
             Console.WriteLine(MapYFrom + "MAPYFROM");
@@ -117,13 +172,75 @@ namespace MandelBotSetPractica
                     X = this.Map(pixelX, 0, MandelBrot.Width, this.MapXFrom, this.MapXTo);
                     Y = this.Map(pixelY, 0, MandelBrot.Height, this.MapYFrom, this.MapYTo);
                     //This gets the mandelgetal from another method that calculates it
+                    mandelGetal = calculateMandelgetal(X, Y);
 
-                    mandelGetal = calculateMandelgetal(X *(0.01 / this.ScaleFactor), Y * (0.01 / this.ScaleFactor));
+                    Color MandelColor;
 
-                    double PreBrigthR = this.Map(mandelGetal /3, 0, MaxLoop /3, 0, 255);
+                    /*
+                    double PreBrigthR = this.Map(mandelGetal, 0, MaxLoop, 0, 255);
                     double PreBrigthG = this.Map(mandelGetal, 0, MaxLoop, 0, 255);
-                    double PreBrigthB = this.Map(mandelGetal /4, 0, MaxLoop /4, 0, 255);
-                    Color MandelColor = Color.FromArgb((int)PreBrigthR, (int)PreBrigthB, (int)PreBrigthG);
+                    double PreBrigthB = this.Map(mandelGetal, 0, MaxLoop, 0, 255);
+                    MandelColor = Color.FromArgb((int)PreBrigthR, (int)PreBrigthB, (int)PreBrigthG);
+                    /*
+                    /*
+                    double map = this.Map(mandelGetal, 0, MaxLoop, 0.4, 1);
+                    MandelColor = HSL2RGB(map, 0.5, 0.5);
+                     if (mandelGetal == MaxLoop)
+                    {
+                        MandelColor = Color.White;
+                    }
+                     
+                     */
+
+                    switch (this.ColorMode) {
+                        case "default":
+                            int RGB = (int)this.Map(mandelGetal, 0, MaxLoop, 0, 255);
+                            if (mandelGetal == MaxLoop)
+                            {
+                                MandelColor = Color.White;
+                            }
+                            MandelColor = Color.FromArgb(RGB, RGB, RGB);
+                            break;
+                        case "yellow":
+                            double map = this.Map(mandelGetal, 0, MaxLoop, 0.4, 1);
+                            MandelColor = HSL2RGB(map, 0.5, 0.5);
+                            if (mandelGetal == MaxLoop)
+                            {
+                                MandelColor = Color.White;
+                            }
+                            break;
+                        case "fancy":
+                            double PreBrigth = this.Map(mandelGetal, 0, MaxLoop, 0, 1);
+                            int Brigth = (int)this.Map(Math.Sqrt(PreBrigth), 0, 1, 0, 255);
+                            if (mandelGetal == MaxLoop)
+                            {
+                                Brigth = 0;
+                            }
+                            MandelColor = Color.FromArgb(Brigth, Brigth, Brigth);
+                            break;
+                        case "zembla":
+                            double map1 = this.Map(mandelGetal, 0, MaxLoop, 0.7, 1);
+                            MandelColor = HSL2RGB(map1, 0.5, 0.5);
+                            if (mandelGetal == MaxLoop)
+                            {
+                                MandelColor = Color.White;
+                            }
+                            break;
+                        default:
+                            double PreBrigthR = this.Map(mandelGetal, 0, MaxLoop, 0, 255);
+                            double PreBrigthG = this.Map(mandelGetal, 0, MaxLoop, 0, 255);
+                            double PreBrigthB = this.Map(mandelGetal, 0, MaxLoop, 0, 255);
+                            if (mandelGetal == MaxLoop)
+                            {
+                                MandelColor = Color.White;
+                            }
+                            MandelColor = Color.FromArgb((int)PreBrigthR, (int)PreBrigthB, (int)PreBrigthG);
+                            break;
+                    }
+
+
+
+
                     MandelBrot.SetPixel(pixelX, pixelY, MandelColor);
                 }
             }
@@ -167,6 +284,139 @@ namespace MandelBotSetPractica
             MandelBrotImg.Refresh();
         }
 
+        // Given H,S,L in range of 0-1
+
+        // Returns a Color (RGB struct) in range of 0-255
+
+        public static Color HSL2RGB(double h, double sl, double l)
+
+        {
+
+            double v;
+
+            double r, g, b;
+
+
+
+            r = l;   // default to gray
+
+            g = l;
+
+            b = l;
+
+            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
+
+            if (v > 0)
+
+            {
+
+                double m;
+
+                double sv;
+
+                int sextant;
+
+                double fract, vsf, mid1, mid2;
+
+
+
+                m = l + l - v;
+
+                sv = (v - m) / v;
+
+                h *= 6.0;
+
+                sextant = (int)h;
+
+                fract = h - sextant;
+
+                vsf = v * sv * fract;
+
+                mid1 = m + vsf;
+
+                mid2 = v - vsf;
+
+                switch (sextant)
+
+                {
+
+                    case 0:
+
+                        r = v;
+
+                        g = mid1;
+
+                        b = m;
+
+                        break;
+
+                    case 1:
+
+                        r = mid2;
+
+                        g = v;
+
+                        b = m;
+
+                        break;
+
+                    case 2:
+
+                        r = m;
+
+                        g = v;
+
+                        b = mid1;
+
+                        break;
+
+                    case 3:
+
+                        r = m;
+
+                        g = mid2;
+
+                        b = v;
+
+                        break;
+
+                    case 4:
+
+                        r = mid1;
+
+                        g = m;
+
+                        b = v;
+
+                        break;
+
+                    case 5:
+
+                        r = v;
+
+                        g = m;
+
+                        b = mid2;
+
+                        break;
+
+                }
+
+            }
+
+            Color rgb;
+
+            int rgbR = Convert.ToByte(r * 255.0f);
+
+            int rgbG = Convert.ToByte(g * 255.0f);
+
+            int rgbB = Convert.ToByte(b * 255.0f);
+
+            rgb = Color.FromArgb(255, rgbR, rgbG, rgbB);
+
+            return rgb;
+
+        }
 
     }
 }
